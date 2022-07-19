3cc63347a59e4694759e4019348e393ab03bb6be
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-07-19 23:13:25 +0100

    add eslint for checking js errors

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const hamburger = document.querySelector("#menu")
-const menu = document.querySelector("#menu2")
-const brandName = document.getElementById("name")
-const navAnchor = document.querySelectorAll('.menu-item')
+const hamburger = document.querySelector('#menu');
+const menu = document.querySelector('#menu2');
+const navAnchor = document.querySelectorAll('.menu-item');
 
-hamburger.addEventListener("click", function () {
-    menu.classList.toggle("active");
-    hamburger.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
 
 navAnchor.forEach(
-    (navAnchors) => {
-      navAnchors.addEventListener('click', function() {
-    menu.classList.toggle("active");
-    hamburger.classList.toggle("active");       
-      });
-    },
-  );
+  (navAnchors) => {
+    navAnchors.addEventListener('click', () => {
+      menu.classList.toggle('active');
+      hamburger.classList.toggle('active');
+    });
+  },
+);
