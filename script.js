b91fedc4a51d7c60c0fc7f7322f8310b8ae8ba93
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-07-19 23:05:50 +0100

    add script for each list item click

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const hamburger = document.querySelector("#menu")
 const menu = document.querySelector("#menu2")
 const brandName = document.getElementById("name")
+const navAnchor = document.querySelectorAll('.menu-item')
 
 hamburger.addEventListener("click", function () {
     menu.classList.toggle("active");
     hamburger.classList.toggle("active");
 })
+
+navAnchor.forEach(
+    (navAnchors) => {
+      navAnchors.addEventListener('click', function() {
+    menu.classList.toggle("active");
+    hamburger.classList.toggle("active");       
+      });
+    },
+  );
