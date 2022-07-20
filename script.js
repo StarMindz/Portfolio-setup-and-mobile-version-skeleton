e231bbd20a86327c4de7f0e1b9db38b1ebaa393e
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-07-20 07:21:22 +0100

    Corrected some linter errors

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector('#menu');
 const menu = document.querySelector('#menu2');
 const navAnchor = document.querySelectorAll('.menu-item');
-
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('active');
   hamburger.classList.toggle('active');
