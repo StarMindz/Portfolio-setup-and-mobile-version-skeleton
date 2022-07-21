c142aa56f758bd942fb83436b8a07f446df5053a
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-07-21 17:57:49 +0100

    Added overlay to appear when popup is activated

--- scripts/popup.js
+++ scripts/popup.js
@@ -16,17 +16,34 @@ var maper = function (name, description, image, technology) {
     popUp.technology[2].innerHTML= technology[2];
 }
 
-maper("Multi Post Stories", 
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+const button7 = document.querySelector("#button7");
+const cancel = document.querySelector("#cancel-pup-up");
+const popUpObject = document.querySelector(".pop-up");
+const overlay = document.querySelector("#overlay");
+
+button1.addEventListener('click', () => {
+    maper("Multi Post Stories", 
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent", 
     "images/Snapshoot Portfolio.png", 
     ["Html", "Bootstrap", "Ruby on Rails"]);
 
+    popUpObject.classList.add('active');
+    overlay.classList.add("active");
+});
 
-const button1 = document.querySelector("#button1")
-const button2 = document.querySelector("#button2")
-const button3 = document.querySelector("#button3")
-const button4 = document.querySelector("#button4")
-const button5 = document.querySelector("#button5")
-const button6 = document.querySelector("#button6")
-const button7 = document.querySelector("#button7")
+cancel.addEventListener('click', () => {
+    popUpObject.classList.remove('active');
+    overlay.classList.remove("active");
+});
 
+overlay.addEventListener('click', () => {
+    popUpObject.classList.remove('active');
+    overlay.classList.remove("active");
+});
